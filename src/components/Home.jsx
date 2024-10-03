b1509da519b59ef70764b522e171cb0ba90b811e
Ruby Izekor <1izekorruby@gmail.com>
2024-10-03 14:32:50 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,19 @@
 import React from "react"
+import { useNavigate } from 'react-router-dom';
+
 
 const Home =()=>{
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const getUserStatus = async () => {
+          const isUser = localStorage.getItem('isUser');
+          return isUser === 'true';
+        };
+        const user = await getUserStatus()
+        if(!user){
+            navigate('/');
+        }
+    })[]
     return (
         <div>
             Home
