41810384563391566d686d6461e73e78fc3987ff
Ruby Izekor <1izekorruby@gmail.com>
2024-10-03 15:47:42 +0100

    Update Auth.jsx form

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -122,7 +122,6 @@ const Login = ({setState})=>{
     setShowPassword(!showPassword);
   };
     return(
-      <form onSubmit={handleSubmit}>
         <div className="mx-auto grid w-[350px] gap-6">
             <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
@@ -175,6 +174,5 @@ const Login = ({setState})=>{
             </span>*/}
             </div>
         </div>
-    </form>
     )
 }
